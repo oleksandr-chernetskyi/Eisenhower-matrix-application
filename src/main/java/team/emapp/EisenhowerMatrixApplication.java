ffa9a07adb21c0348cc89fe061d9992fc1bf2923
Alexander <liero2D@gmail.com>
2024-02-09 23:50:29 +0200

    first commit

---
+++ src/main/java/team/emapp/EisenhowerMatrixApplication.java
@@ -0,0 +1,13 @@
+package team.emapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisenhowerMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EisenhowerMatrixApplication.class, args);
+	}
+
+}
